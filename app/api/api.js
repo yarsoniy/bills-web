7ed9fc574eb42778e858c99afefd2f67e7203dee
Yaroslav Sorochynskyi <yarsoniy@gmail.com>
2024-03-17 13:32:43 +0200

    Refactor api module

--- app/api/api.js
+++ app/api/api.js
@@ -1,33 +1,39 @@
-const getAllGroups = async () => {
-  const response = await fetch('/api/v1/participant_group');
-  const data = await response.json();
-  return data.data;
-}
-
-const getGroup = async (groupId) => {
-  const response = await fetch('/api/v1/participant_group/'+groupId);
-  const data = await response.json();
-  return data.data;
-}
-
-const createGroup = async (title) => {
-  const response = await fetch('/api/v1/participant_group/', {
-    method: 'POST',
-    body: JSON.stringify({title: title})
-  });
-  if (response.status !== 200) {
-    throw new Error('Request failed');
+export const api = {
+  getAllGroups: async() => {
+    const response = await fetch('/api/v1/participant_group');
+    const data = await response.json();
+    return data.data;
+  },
+  getGroup: async(groupId) => {
+    const response = await fetch('/api/v1/participant_group/'+groupId);
+    const data = await response.json();
+    return data.data;
+  },
+  createGroup: async(title) => {
+    const response = await fetch('/api/v1/participant_group/', {
+      method: 'POST',
+      body: JSON.stringify({title: title})
+    });
+    if (response.status !== 200) {
+      throw new Error('Request failed');
+    }
+  },
+  createParticipant: async(groupId, name) => {
+    const response = await fetch('/api/v1/participant_group/'+groupId+'/participant', {
+      method: 'POST',
+      body: JSON.stringify({name: name})
+    });
+    if (response.status !== 200) {
+      throw new Error('Request failed');
+    }
+  },
+  createBill: async(groupId, title) => {
+    const response = await fetch('/api/v1/participant_group/'+groupId+'/bill', {
+      method: 'POST',
+      body: JSON.stringify({title: title})
+    });
+    if (response.status !== 200) {
+      throw new Error('Request failed');
+    }
   }
 }
-
-const createParticipant = async (groupId, name) => {
-  const response = await fetch('/api/v1/participant_group/'+groupId+'/participant', {
-    method: 'POST',
-    body: JSON.stringify({name: name})
-  });
-  if (response.status !== 200) {
-    throw new Error('Request failed');
-  }
-}
-
-export const api = {getAllGroups, getGroup, createGroup, createParticipant};
